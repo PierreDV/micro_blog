15364ac7fb03b8c8c63c9c751bc8ba92bd7efcd3
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-16 10:21:00 -0400

    added contact page and red carpet gem for markdown filter

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,23 @@
 class StaticPagesController < ApplicationController
   def home
+    set_base_title
   end
 
   def help
+    set_base_title
   end
 
   def about
+    set_base_title
+  end
+
+  def contact
+    set_base_title
+  end
+
+  private
+  
+  def set_base_title
+    @base_title = "Micro Blog"
   end
 end
